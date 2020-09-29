62502786b3b28222edcd68b69af012e178e1b4bc
USER <e.kelidari73@gmail.com>
2020-09-29 12:41:33 +0330

    added simple login

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,9 @@ from .  import views
 
 app_name= 'accounts'
 urlpatterns= [
-    path('', views.signup_view , name='signup'),
+    path('signup', views.signup_view , name='signup'),
+    path('login', views.login_view , name='login'),
+    path('logout', views.logout_view , name='logout'),
+
+
 ]
